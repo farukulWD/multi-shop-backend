4ac0aa48a2a9983eaeb00d8cc391821f1deb8089
farukulWD <farukgb1999@gmail.com>
2025-06-02 17:10:39 +0600

    authentication function added

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -4,13 +4,27 @@ import httpStatus from "http-status";
 import { User } from "./user.model";
 
 const createUser = async (userData: IUser) => {
-  console.log({userData})
+  console.log({ userData });
   if (!userData) {
     throw new AppError(httpStatus.BAD_REQUEST, "User data is required");
   }
 
   if (!userData.username || !userData.password || !userData.shops) {
     throw new AppError(httpStatus.BAD_REQUEST, "All fields are required");
+  }
+  if (!userData.shops || userData.shops.length < 3) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "At least 3 shop names required"
+    );
+  }
+
+  const existing = await User.findOne({ shops: { $in: userData.shops } });
+  if (existing) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "One or more shop names already taken"
+    );
   }
 
   const existingUser = await User.userFindByUserName(userData.username);
