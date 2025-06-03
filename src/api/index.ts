006129547b2389fd061f8a6e58d8523df6193be7
farukulWD <farukgb1999@gmail.com>
2025-06-03 12:02:02 +0600

    feat: update API handler to use HTTP server and modify Vercel configuration

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,9 @@
 // api/index.ts
-import app from "../app";
-import serverless from "serverless-http";
 
-export const handler = serverless(app);
+import app from '../app';
+import { createServer } from 'http';
+
+module.exports = (req: any, res: any) => {
+  const server = createServer(app);
+  return server.emit('request', req, res);
+};
